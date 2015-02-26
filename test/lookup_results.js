e42699ad691e48c70840af19e9b2247696a48005
Severyn Kozak <severyn.kozak@gmail.com>
2015-02-26 10:49:32 -0500

    Add a lookup speed test.
    test/lookup_results.js
    	-Add a test that reports the speed of the admin lookup.
    	-Useful for testing it against different levels of
    	simplification.

--- test/lookup_results.js
+++ test/lookup_results.js
@@ -10,8 +10,25 @@ var supervisor = require( '../');
 var lookupPoints = require( './lookup_points.json' );
 var util = require( 'util' );
 
-tape( 'Test actual lookup results against expected.', function ( test ){
-  supervisor.lookup( function ( lookup ){
+supervisor.lookup( function ( lookup ){
+  /**
+   * A wrapper for `test.end()` that closes the admin-lookup when all tests
+   * have completed execution.
+   */
+  var numTestsCompleted = 0;
+  var totalNumTests = 2;
+  function completeTest( test ){
+    if( ++numTestsCompleted === totalNumTests ){
+      console.log( 'ended' );
+      lookup.end();
+    }
+    test.end();
+  }
+
+  /**
+   * Test lookup results for the points in `./lookup_points.json`.
+   */
+  tape( 'Test actual lookup results against expected.', function ( test ){
     var numCompletedTestCases = 0;
 
     function lookupTestCase( testCase ){
@@ -33,12 +50,50 @@ tape( 'Test actual lookup results against expected.', function ( test ){
         }
 
         if( ++numCompletedTestCases === lookupPoints.length ){
-          lookup.end();
-          test.end();
+          completeTest( test );
         }
       });
     }
 
     lookupPoints.forEach( lookupTestCase );
+  });
+
+  /**
+   * Not quite a unit-test, but reports the amount of time to taken to lookup a
+   * large number of points.
+   */
+  tape( 'Test lookup speed.', function ( test ){
+    function timeMs(){
+      return new Date().getTime();
+    }
+
+    var numLookupPoints = 50000;
+    var points = [];
+    for( var pt = 0; pt < numLookupPoints; pt++ ){
+      points.push({
+        lat: 85 * (Math.random() * 2 - 1),
+        lon: 180 * (Math.random() * 2 - 1)
+      });
+    }
+
+    var numCompleted = 0;
+    var startTime = timeMs();
+    function searchPoint( pt ){
+      lookup.search( pt, function (){
+        if( ++numCompleted === numLookupPoints ){
+          var timeTaken = timeMs() - startTime;
+          test.pass( util.format(
+            'Total time taken (ms) for %d points: %s',
+            numLookupPoints, timeTaken
+          ));
+          test.pass(
+            'Average time per point (ms): ' + timeTaken / numLookupPoints
+          );
+          completeTest( test );
+        }
+      });
+    }
+
+    points.forEach( searchPoint );
   });
 });
